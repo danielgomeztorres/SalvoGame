1237ab04e01c97e85065f653713fd34607fed617
dani <space.dg23@gmail.com>
2019-02-07 17:56:56 +0100

    SalvoGame

--- salvo/src/main/resources/static/web/game.js
+++ salvo/src/main/resources/static/web/game.js
@@ -17,6 +17,10 @@ var app = new Vue({
 		nameShip: null,
 		father: null,
 		child: null,
+		idCellHor: null,
+		paint: [],
+		row: false,
+		column: false,
 		finalPosition: [{
 
 				shipPositions: "Horizontal",
@@ -48,7 +52,7 @@ var app = new Vue({
 
 
 		],
-		
+
 
 
 
@@ -163,7 +167,6 @@ var app = new Vue({
 					positionShips.classList.add("boxShips");
 				}
 			}
-
 			for (var gamePlayer in this.salvoes) { //	console.log(gamePlayer);
 				for (var key in this.salvoes[gamePlayer]) {
 					var salvo = this.salvoes[gamePlayer][key]; //console.log([key])
@@ -175,8 +178,6 @@ var app = new Vue({
 						} else {
 							var shotsOponnent = document.getElementById(salvo[x]); //console.log(shotsOponnent)//console.log(positionShips)
 							for (var z = 0; z < this.datos.Ships.length; z++) {
-								// for (var s = 0; s < this.datos.Ships[z].location.length; s++){// 	var ships = this.datos.Ships[z].location[s];
-								// 	if(salvo[x] == ships ) {// 	var shipsElement = document.getElementById(ships);// 	shipsElement.classList.remove("boxShips");//     shipsElement.classList.add("boxSalvoYo");// 	}// }
 								if (this.datos.Ships[z].location.includes(salvo[x])) { //con el includes ahorramos el segundo for que esta comentado arriba
 									var shipsElement = document.getElementById(salvo[x]);
 									shipsElement.classList.remove("boxShips");
@@ -211,29 +212,30 @@ var app = new Vue({
 					}
 				}
 			}
-			console.log(this.currentShip)
-
+			// this.dragEnter();
+			// this.drop(this.father.parentNode.id);
 		},
 		allowDrop: function (ev) {
 			ev.preventDefault();
-			//console.log(ev)
 		},
 		drag: function (ev) { // GUARDAR EL BARCO CUANDO CLICKAS
-
 			console.log("drag", ev)
 			this.father = ev.target
+			this.father2 = ev.target
 			console.log("div padre: ", this.father)
 			this.child = ev.target.childElementCount;
 			console.log("numero de hijos; ", this.child)
 			this.nameShip = ev.target.id;
 			console.log("nombre del barco: ", this.nameShip)
-			
 		},
 		dragEnter: function (ev) {
-			console.log("dragEnter", ev)
-
+			var cell = document.getElementsByClassName("box")
+			for (var l = 0; l < cell.length; l++) {
+				cell[l].classList.remove("boxGreen")
+				cell[l].classList.remove("boxRed")
+			}
+			/////////////////////////////////////////////////////VERTICAL
 			if (this.currentShip == "Vertical") {
-				
 				ev.preventDefault();
 				this.saveArrayVer = [];
 				var firstLetter = ev.target.id.slice(0, 1) //first position
@@ -242,41 +244,86 @@ var app = new Vue({
 					var lastLetter = startLetter + (Number(this.child) - 1)
 				}
 				for (var k = startLetter; k <= lastLetter; k++) { //Loop para determinar la posicion de las letras inicial en los barcos verticales
-					this.saveArrayVer.push(this.letters[k] + (Number(ev.target.id.slice(1, 2))));
+					let idCellVer = this.letters[k] + (Number(ev.target.id.slice(1, 3)))
+					let paint2 = document.getElementById(idCellVer)
+					console.log(paint2)
+					 if (!paint2) {
+						this.column = true 
+						 console.log(this.column)
+						 break	 
+					 } else { 
+						this.column = false
+						 console.log(this.column)	 	 
+					 }
+					}
+					 for (var k = startLetter; k <= lastLetter; k++) { //Loop para determinar la posicion de las letras inicial en los barcos verticales
+						let idCellVer = this.letters[k] + (Number(ev.target.id.slice(1, 3)))
+						let paint2 = document.getElementById(idCellVer)
+					//paint.classList.add("boxGreen")
+					this.saveArrayVer.push(idCellVer);
+					if (this.column) {	
+						paint2.classList.add("boxRed")
+						this.father = [] 
+					} else if (!this.column) {
+						paint2.classList.add("boxGreen")
+					}
+				
 				}
-
-				console.log(this.saveArrayVer);
+			/////////////////////////////////////////////////////HORIZONTAL
 			} else if (this.currentShip == "Horizontal") {
-				this.saveArrayHor = [];
 				ev.preventDefault();
+				this.saveArrayHor = [];
 				for (var i = 0; i < Number(this.child); i++) {
-					this.saveArrayHor.push(ev.target.id.slice(0, 1) + (Number(ev.target.id.slice(1, 2)) + i))
-					console.log(" save horizontal", this.saveArrayHor)
+					let idCellHor = ev.target.id.slice(0, 1) + (Number(ev.target.id.slice(1, 3)) + i)
+					let paint = document.getElementById(idCellHor)
+					console.log(paint)
+					if (!paint) {
+						this.row = true;
+						console.log(this.row)
+						break;
+					} else {
+						this.row = false;
+						console.log(this.row)
+					}
 				}
-				console.log("arrayhor", this.saveArrayHor);
-
+				for (var i = 0; i < Number(this.child); i++) {
+					let idCellHor = ev.target.id.slice(0, 1) + (Number(ev.target.id.slice(1, 3)) + i)
+					let paint = document.getElementById(idCellHor)
+					this.saveArrayHor.push(idCellHor)
+					if (this.row) {
+						paint.classList.add("boxRed")
+						this.father = [] 
+					
+					} else if (!this.row) {
+						paint.classList.add("boxGreen")
+					}
+				}
 			}
+			this.father = this.father2 
 			console.log("final Position", this.finalPosition);
 			ev.dataTransfer.setData("el", ev.target.id);
-			//var currentCell = document.getElementById(ev.target.id)
-			//console.log("celda del puntero :", currentCell)
 		},
 		// dragEnd: function (ev) {
 		// 	ev.preventDefault();
 		// 	//ev.dataTransfer.setData("el", ev.target.id);
 		// },
 		drop: function (ev) {
-			console.log("drog", ev)
-			ev.target.appendChild(this.father);
+			this.father =
+				ev.target.appendChild(this.father);
 			for (var x = 0; x < this.finalPosition.length; x++) {
 				if (this.finalPosition[x].shipName == this.nameShip) {
-
+					//this.finalPosition[x].shipLocations = []
 					if (this.saveArrayVer.length > 0) {
 						console.log("vertical")
+
 						this.finalPosition[x].shipLocations = this.saveArrayVer
+
+
+
 					} else if (this.saveArrayHor.length > 0) {
 						console.log("horizontal")
 						this.finalPosition[x].shipLocations = this.saveArrayHor
+
 					}
 				}
 			}
@@ -284,6 +331,7 @@ var app = new Vue({
 			this.currentShip = "Horizontal"
 			this.saveArrayHor = [];
 			this.saveArrayVer = [];
+	
 		},
 
 	},
@@ -292,5 +340,6 @@ var app = new Vue({
 
 	}
 });
+
 
 
